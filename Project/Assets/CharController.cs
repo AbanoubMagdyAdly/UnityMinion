e9fe20efb133060a6a73caec4c32125e1026054c
AbanoubMagdyAdly <Abanoub.Magdy.Adly@gmail.com>
2019-02-24 18:24:36 +0200

    first play

---
+++ Project/Assets/CharController.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharController : MonoBehaviour
+{
+    private Animator anim;
+    public float speed = 10f;
+    Rigidbody2D rigidbody2d;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        rigidbody2d = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (null != anim)
+            {
+                rigidbody2d.velocity = new Vector3(0, speed * 1,0);
+                // play Bounce but start at a quarter of the way though
+                anim.Play("Walk", 0, 0.25f);
+            }
+        }
+    }
+
+    void FixedUpdate()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            rigidbody2d.velocity = new Vector2(speed * -1, 0f);
+            anim.SetBool("Idle", false);
+            anim.SetBool("Walk", true);
+        }
+
+
+
+        else if (Input.GetKey(KeyCode.RightArrow))
+        {
+            rigidbody2d.velocity = new Vector2(speed, 0f);
+            anim.SetBool("Walk", true);
+            anim.SetBool("Idle", false);
+
+        }
+
+        else
+        {
+           
+            anim.SetBool("Idle", true);
+            anim.SetBool("Walk", false);
+
+        }
+
+    }
+}
---
+++ Project/Assets/walk.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    void Update()
+    {
+
+        // Move the Character:
+        transform.Translate(Input.GetAxis("Horizontal") * 15f * Time.deltaTime, 0f, 0f);
+
+        // Flip the Character:
+        Vector3 characterScale = transform.localScale;
+            characterScale.x = -10;
+        transform.localScale = characterScale;
+    }
+    private void OnMouseDrag()
+    {
+        this.Update();
+    }
+}
